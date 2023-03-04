b46c3748dec0a7e3d29c768823956d86859dbe2a
Juntao Qiu <juntao.qiu@gmail.com>
2023-03-04 14:04:03 +1100

    init the repo with a working example

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
-function App() {
+import axios from "axios";
+
+const API_KEY = process.env.REACT_APP_OPENAI_KEY || "";
+const model = "text-davinci-002";
+
+const fetchData = async (input: string) => {
+  const response = await axios.post(
+    "https://api.openai.com/v1/completions",
+    {
+      prompt: `Complete this sentence: "${input}"`,
+      model: model,
+      max_tokens: 50,
+      n: 1,
+      stop: ".",
+    },
+    {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${API_KEY}`,
+      },
+    }
+  );
+
+  return response.data.choices[0].text;
+};
+
+function ChattyApp() {
+  const [input, setInput] = useState("");
+  const [completedSentence, setCompletedSentence] = useState("");
+
+  async function handleClick() {
+    try {
+      const completedSentence = await fetchData(input);
+      setCompletedSentence(completedSentence);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h2>Tell me something, and I'll tell you more</h2>
+      <textarea
+        value={input}
+        onChange={(event) => setInput(event.target.value)}
+        rows={5}
+        placeholder="Type in some words and I'll finish the rest..."
+      />
+      <button className="button" onClick={handleClick}>
+        Complete Sentence
+      </button>
+      {completedSentence && <p>Completed sentence: {completedSentence}</p>}
     </div>
   );
 }
 
-export default App;
+export default ChattyApp;
